e1ad461d0c32bd7bcf9736550833201ef3e11e56
mazi <602281666@qq.com>
2021-08-17 11:15:13 +0800

    commit

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
+import { join } from 'path'
+
 import Koa from 'koa'
 import { ApolloServer, AuthenticationError, gql, UserInputError } from 'apollo-server-koa'
 import { DocumentNode, GraphQLScalarType, Kind } from 'graphql'
 import { IResolvers } from '@graphql-tools/utils'
+import { makeExecutableSchema } from '@graphql-tools/schema'
 import { BaseRedisCache } from 'apollo-server-cache-redis'
 import Redis from 'ioredis'
 import { GraphQLUpload, graphqlUploadKoa } from 'graphql-upload'
 import promises from 'stream/promises'
+import { GraphQLSchemaModule } from 'apollo-server-core'
+import { loadSchemaSync } from '@graphql-tools/load'
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 
 // import MoviesAPI from './datasource/movies'
 // import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core/dist/plugin/landingPage/graphqlPlayground'
@@ -17,78 +23,83 @@ import promises from 'stream/promises'
 
 const PORT = 8899
 
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+const typeDefs = loadSchemaSync(join(__dirname, 'gql/*.gql'), {
+  loaders: [new GraphQLFileLoader()]
+})
 
-  "Description for the type"
-  type Book {
-    """
-    Description for field
-    Supports **multi-line** description for your [API](http://example.com)!
-    """
-    title: String
-    publishData: Date
-    author: String
-    oldTitle: String @deprecated(reason: "Use \`title\`")
-  }
 
-  scalar Date
+// const typeDefs = gql`
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  scalar Odd
+//   "Description for the type"
+//   type Book {
+//     """
+//     Description for field
+//     Supports **multi-line** description for your [API](http://example.com)!
+//     """
+//     title: String
+//     publishData: Date
+//     author: String
+//     oldTitle: String @deprecated(reason: "Use \`title\`")
+//   }
 
-  scalar Upload
+//   scalar Date
 
-  directive @withDeprecatedArgs(
-    deprecatedArg: String @deprecated(reason: "Use \`newArg\`"),
-    newArg: String
-  ) on FIELD
+//   scalar Odd
 
-  type File {
-    filename: String!
-    mimetype: String!
-    encoding: String!
-  }
+//   scalar Upload
 
-  type MyType {
-    # ARGUMENT_DEFINITION (alternate example on a field's args)
-    # fieldWithDeprecatedArgs(name: String! @deprecated): String // this is an error example
-    fieldWithDeprecatedArgs(name: String @deprecated): String
-    # FIELD_DEFINITION
-    deprecatedField: String @deprecated
-  }
+//   directive @withDeprecatedArgs(
+//     deprecatedArg: String @deprecated(reason: "Use \`newArg\`"),
+//     newArg: String
+//   ) on FIELD
 
-  enum MyEnum {
-    # ENUM_VALUE
-    OLD_VALUE @deprecated(reason: "Use \`NEW_VALUE\`.")
-    NEW_VALUE
-  }
+//   type File {
+//     filename: String!
+//     mimetype: String!
+//     encoding: String!
+//   }
 
-  input SomeInputType {
-    nonDeprecated: String
-    # INPUT_FIELD_DEFINITION
-    deprecated: String @deprecated
-  }
+//   type MyType {
+//     # ARGUMENT_DEFINITION (alternate example on a field's args)
+//     # fieldWithDeprecatedArgs(name: String! @deprecated): String // this is an error example
+//     fieldWithDeprecatedArgs(name: String @deprecated): String
+//     # FIELD_DEFINITION
+//     deprecatedField: String @deprecated
+//   }
 
-  type Movie {
-    name: String
-    author: String
-  }
+//   enum MyEnum {
+//     # ENUM_VALUE
+//     OLD_VALUE @deprecated(reason: "Use \`NEW_VALUE\`.")
+//     NEW_VALUE
+//   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-    echoOdd(odd: Odd!): Odd!
-    # movie(id: Int!): Movie
-    # mostViewedMovies: [Movie]
-    otherFields: Boolean!
-  }
+//   input SomeInputType {
+//     nonDeprecated: String
+//     # INPUT_FIELD_DEFINITION
+//     deprecated: String @deprecated
+//   }
 
-  type Mutation {
-    singleUpload(file: Upload!): File!
-  }
-`
+//   type Movie {
+//     name: String
+//     author: String
+//   }
+
+//   # The "Query" type is special: it lists all of the available queries that
+//   # clients can execute, along with the return type for each. In this
+//   # case, the "books" query returns an array of zero or more Books (defined above).
+//   type Query {
+//     books: [Book]
+//     echoOdd(odd: Odd!): Odd!
+//     # movie(id: Int!): Movie
+//     # mostViewedMovies: [Movie]
+//     otherFields: Boolean!
+//   }
+
+//   type Mutation {
+//     singleUpload(file: Upload!): File!
+//   }
+// `
 
 function oddValue(value: number) {
   if (typeof value === 'number' && Number.isInteger(value) && value % 2 !== 0) {
@@ -175,10 +186,18 @@ interface ServerApp {
   app: Koa
 }
 
+const modules: GraphQLSchemaModule[] = [
+  {
+    typeDefs,
+    resolvers: {},
+  }
+]
+
 async function startApolloServer(typeDefs: DocumentNode, resolvers: IResolvers, port: number): Promise<ServerApp> {
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    modules,
+    // typeDefs,
+    // resolvers,
     cache: new BaseRedisCache({
       client: new Redis({
         host: '127.0.0.1',
